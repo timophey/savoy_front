7192409cd0858fc60536594f96ec925eff20155b
Timophey Lanevich <timophey_@mail.ru>
2023-12-26 14:15:16 +0500

    20231226_morning

--- build/js/main.js
+++ build/js/main.js
@@ -355,10 +355,12 @@ function initMainMap() {
   if (!node) return;
   var arPoints = [{
     name: 'Чистые пруды',
-    latlng: [55.7666, 37.638495]
+    latlng: [55.7666, 37.638495],
+    link: 'https://yandex.ru/maps/org/savoy_chistyye_prudy/6353151837/'
   }, {
     name: 'Новослободская',
-    latlng: [55.775626, 37.601317]
+    latlng: [55.775626, 37.601317],
+    link: 'https://yandex.ru/maps/org/savoy_italyanskiy_kvartal/61406058907/'
   }];
   var mapCenter = [arPoints.map(function (item) {
     return item.latlng[0];
@@ -376,22 +378,65 @@ function initMainMap() {
     center: mapCenter,
     zoom: 13,
     type: 'yandex#map'
+    // controls: ['zoomControl', 'searchControl', 'typeSelector',  'fullscreenControl', 'routeButtonControl']
     // adjustZoomOnTypeChange: true
+  }, {
+    // searchControlProvider: 'yandex#search'
   });
+  map.controls.add('zoomControl', {
+    float: 'right'
+    // position: {
+    //     left: 40,
+    //     top: 'center'
+    // }
+  });
+  map.geoObjects.events.add('click', function (event) {
+    location.href = event.get('target').properties.get('link');
+  });
+  var adaptives = {
+    0: {
+      center: mapCenter,
+      zoom: 13,
+      width: 0
+    },
+    1024: {
+      center: [55.763248, 37.691611],
+      zoom: 12,
+      width: 1024
+    },
+    1280: {
+      center: [55.770661, 37.665679],
+      zoom: 13,
+      width: 1024
+    }
+  };
+  var timerResize = 0;
+  var doResize = function doResize() {
+    var geometry = {};
+    for (var key in adaptives) {
+      if (+key <= window.innerWidth) {
+        geometry = adaptives[key];
+      }
+    }
+    console.log(geometry);
+    map.setCenter(geometry.center, geometry.zoom);
+  };
   arPoints.forEach(function (item) {
     var mark = new ymaps.Placemark(item.latlng, {
       // iconContent: 'Москва', 
       hintContent: item.name,
-      hintFitPane: false
+      hintFitPane: false,
+      link: item.link
     }, {
       iconLayout: 'default#image',
       iconImageHref: 'images/icon_mark.svg',
       iconImageSize: [32, 32],
       hideIconOnBalloonOpen: false
     });
-    var hint1 = new ymaps.Hint(item.name, item.latlng, {
-      // maxWidth: 200
-    });
+
+    // var hint1 = new ymaps.Hint(item.name, item.latlng, {
+    //     // maxWidth: 200
+    //     });
     console.log(map);
     // map.hint.add(hint1);
     // mark.hint.open();
@@ -404,6 +449,11 @@ function initMainMap() {
     // })
 
     map.geoObjects.add(mark);
+  });
+  doResize();
+  window.addEventListener('resize', function () {
+    clearTimeout(timerResize);
+    timerResize = setTimeout(doResize, 500);
   });
 
   // mouseenter
--- src/js/main_map.js
+++ src/js/main_map.js
@@ -4,11 +4,13 @@ function initMainMap(){
     let arPoints = [
         {
             name:'Чистые пруды',
-            latlng:[55.7666,37.638495,]
+            latlng:[55.7666,37.638495,],
+            link: 'https://yandex.ru/maps/org/savoy_chistyye_prudy/6353151837/',
         },        
         {
             name:'Новослободская',
             latlng:[55.775626,37.601317,],
+            link: 'https://yandex.ru/maps/org/savoy_italyanskiy_kvartal/61406058907/',
         }
     ];
 
@@ -25,14 +27,61 @@ function initMainMap(){
         center: mapCenter,
         zoom: 13,
         type: 'yandex#map',
+        // controls: ['zoomControl', 'searchControl', 'typeSelector',  'fullscreenControl', 'routeButtonControl']
         // adjustZoomOnTypeChange: true
+    },{
+        // searchControlProvider: 'yandex#search'
     });
+
+    map.controls.add('zoomControl', {
+        float: 'right',
+        // position: {
+        //     left: 40,
+        //     top: 'center'
+        // }
+    });
+
+    map.geoObjects.events.add('click', function (event) {
+        location.href = event.get('target').properties.get('link');
+    });
+
+    let adaptives = {
+        0: {
+            center: mapCenter,
+            zoom: 13,
+            width: 0,
+        },
+        1024: {
+            center: [55.763248,37.691611,],
+            zoom: 12,
+            width: 1024,
+        },
+        1280: {
+            center: [55.770661,37.665679,],
+            zoom: 13,
+            width: 1024,
+        },
+    }
+
+    let timerResize = 0;
+    let doResize = function(){
+        
+        let geometry = {}
+        for(var key in adaptives){
+            if(+key <= window.innerWidth){
+                geometry = adaptives[key];
+            }
+        }
+        console.log(geometry)
+        map.setCenter(geometry.center,geometry.zoom );
+    }
 
     arPoints.forEach(item => {
         let mark = new ymaps.Placemark( item.latlng, { 
             // iconContent: 'Москва', 
             hintContent: item.name,
             hintFitPane: false,
+            link: item.link,
         },{
             iconLayout: 'default#image',
             iconImageHref: 'images/icon_mark.svg',
@@ -40,9 +89,9 @@ function initMainMap(){
             hideIconOnBalloonOpen: false,
         });
 
-        var hint1 = new ymaps.Hint(item.name, item.latlng, {
-            // maxWidth: 200
-            });
+        // var hint1 = new ymaps.Hint(item.name, item.latlng, {
+        //     // maxWidth: 200
+        //     });
             console.log(map)
         // map.hint.add(hint1);
         // mark.hint.open();
@@ -53,11 +102,17 @@ function initMainMap(){
         // mark.events.add('mouseleave', function(e){
         //     mark.balloon.close();
         // })
-
         
 
         map.geoObjects.add(mark);
     });
+
+    doResize();
+
+    window.addEventListener('resize',()=>{
+        clearTimeout(timerResize);
+        timerResize = setTimeout(doResize,500);
+    })
 
     
     // mouseenter
